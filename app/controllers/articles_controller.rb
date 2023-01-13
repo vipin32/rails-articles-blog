e767f0ba9174e12bdaa074b36fe1772d8a2c46b1
vipin <champvip32@gmail.com>
2023-01-13 12:00:00 +0530

    Articles CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,67 @@
+class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @articles = Article.all 
+    end
+
+    def show
+    end
+
+    def new
+        @article = Article.new 
+    end
+
+    def create
+
+        @article = Article.new(article_params)
+
+        if @article.save
+            flash[:success] = "Article saved successfully!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+
+        # render plain: params[:article].inspect
+    end
+
+
+    def edit
+    end
+
+    def update
+        
+        if @article.update(article_params)
+            flash[:success] = "Article updated successfully!"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy
+
+        if @article.destroy
+            flash[:success] = "Article deleted successfully"
+        else
+            flash[:notice] = "Something went wrong while deleting article"
+        end
+
+        redirect_to articles_path
+
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id]) 
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+end

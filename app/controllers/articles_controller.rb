c774d373866eaa6dd2d824db860f38c9320a3d21
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2023-02-17 13:16:32 +0530

    completed authentication and authorization for articles and users actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
     
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -16,7 +18,7 @@ class ArticlesController < ApplicationController
     def create
 
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article saved successfully!"
             redirect_to article_path(@article)
@@ -62,6 +64,13 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description, :user_id)
+    end
+
+    def require_same_user
+        if logged_in? && current_user != @article.user
+            flash[:danger] = "You must be the owner of the article to perform any action"
+            redirect_to root_path
+        end
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+    before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_same_user, only: [:edit,:update]
 
     def index
         @users = User.all
@@ -50,5 +50,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit your own account"
+            redirect_to root_path
+        end
     end
 end

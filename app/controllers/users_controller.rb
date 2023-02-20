cd5f98df24e8d390af01e9073fc5085312dcd413
vipin <champvip32@gmail.com>
2023-02-20 10:19:32 +0530

    login user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-
         if @user.save
+            session[:user_id] = @user.id
+
             flash[:success] = "Welcome to Rails blog #{@user.username}!"
             redirect_to articles_path
         else
